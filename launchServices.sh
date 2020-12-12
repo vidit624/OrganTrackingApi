cda4e35eeb3c9f7a3d147afc37cbce9f9203cc52
vidit624 <gpta.vidit1234@iitkgp.ac.in>
2020-12-12 22:17:29 +0530

    Api Files for The Blockchain Network

--- launchServices.sh
+++ launchServices.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export NO_PROXY="*.vectorcars.com,*.carscm.net,*.jamesdistributers.net,*.wbrta.gov.in"
 rm *.log
 nohup ./xclient -p=9090 ../network/connection-profile-vc.yaml > vcapi.log 2>&1 &
 nohup ./xclient -p=9091 ../network/connection-profile-wbrta.yaml > wbrtaapi.log 2>&1 &
